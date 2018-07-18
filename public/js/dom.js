7646ae5094ac14a32dab3518cee8df226a1bd995
othman-shamla <othmansh2010@gmail.com>
2018-07-18 11:35:09 +0300

    add

--- public/js/dom.js
+++ public/js/dom.js
@@ -2,6 +2,7 @@
 addListener("#search", "click", function(event) {
   event.preventDefault();
   select("#main").innerHTML = "";
+  select(".movie_details").innerHTML = "";
   var sec = select("#sec");
   for (let index = sec.length - 1; index > 1; index--) {
     sec.removeChild(sec.childNodes[sec.childNodes.length - 1]);
@@ -10,6 +11,7 @@ addListener("#search", "click", function(event) {
     var url =
       "https://yts.am/api/v2/list_movies.json?query_term=" +
       select("#input").value;
+      
     fetchApi(url, function(response) {
       if (response.data.movie_count != 0) {
         var mov = response.data.movies.length;
@@ -18,6 +20,71 @@ addListener("#search", "click", function(event) {
           var movName = response.data.movies[i].title;
           var movYear = response.data.movies[i].year;
           var image = createlement("img");
+          image.setAttribute("id_movie",response.data.movies[i].id);
+
+          image.addEventListener("click",function(e){
+
+
+            console.log("console")
+            url = "https://yts.am/api/v2/movie_details.json?movie_id="+this.getAttribute("id_movie");
+            fetchApi(url,function(obj){
+              select("#main").innerHTML = "";
+              select(".movie_details").innerHTML = "";
+
+              console.log("console222");
+              var movie = obj.data.movie;
+
+              //mainDiv
+              var mainDiv = select(".movie_details");
+              console.log(mainDiv);
+              
+
+              //firstDiv
+              var imgDiv = createlement("div");
+              imgDiv.classList.add("img");
+              
+              //seacandDiv
+              var infoDiv = createlement("div");
+              infoDiv.classList.add("information");
+              
+              var img = createlement("img");
+              img.src = movie.medium_cover_image;
+              var form = createlement("form");
+              
+              form.setAttribute('method',"get");
+              form.setAttribute('action',movie.torrents[0].url);
+
+              var btn = createlement("button");
+              btn.id = "btn-download"
+              btn.setAttribute('type',"submit");
+              btn.textContent= "Download"
+
+              form.appendChild(btn);
+              
+              //adding elements to first Div;
+              imgDiv.appendChild(img);
+              imgDiv.appendChild(form);
+              mainDiv.appendChild(imgDiv);
+              
+
+              var headingElement = createlement("h1");
+              headingElement.textContent = movie.title;
+
+              var dateElement = createlement("span");
+              dateElement.textContent = movie.year;
+
+              var pElement = createlement("p");
+              pElement.textContent = movie.description_intro;
+              
+              //adding elements to secand Div;
+              infoDiv.appendChild(headingElement);
+              infoDiv.appendChild(dateElement);
+              infoDiv.appendChild(pElement);
+              mainDiv.appendChild(infoDiv);
+              
+            })
+          })
+          
           var movName1 = createlement("h4");
           var movYear1 = createlement("span");
           var scop_movi = createlement("div");
