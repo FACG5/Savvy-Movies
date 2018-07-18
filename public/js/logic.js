e1525343e8cdab0eb71a4b68fd4e662c0bf31216
lubnaabad <lubna_mosa@hotmail.com>
2018-07-18 08:47:25 +0300

    desgin

--- public/js/logic.js
+++ public/js/logic.js
@@ -1,18 +1,15 @@
-
-
-
 function fetch(url, cb) {
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function() {
-      if (xhr.readyState == 4 && xhr.status == 200) {
-        var res = JSON.parse(xhr.responseText);
-        cb(res);
-      }
-    };
-    xhr.open("GET", url, true);
-    xhr.send();
-  }
-
-  if (typeof module !== 'undefined') {
-    module.exports = fetch;
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+      var res = JSON.parse(xhr.responseText);
+      cb(res);
     }
+  };
+  xhr.open("GET", url, true);
+  xhr.send();
+}
+
+if (typeof module !== "undefined") {
+  module.exports = fetch;
+}
